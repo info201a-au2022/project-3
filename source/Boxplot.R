7084da1419e16c4640d3a67a8fef1a7ad36d6105
viacooper <coopoli@uw.edu>
2022-11-16 21:31:12 -0800

    pull

--- source/Boxplot.R
+++ source/Boxplot.R
@@ -1,6 +1,7 @@
 # boxplot
 
-HIV_AIDS_dataset <- read_csv("../data/HIV_AIDS_dataset.csv")
+HIV_AIDS_dataset <- read.csv("../data/HIV_AIDS_dataset.csv")
+
 
 ggplot(HIV_AIDS_dataset, aes(x=as.factor(Country), y=Count_median)) + 
   geom_boxplot(fill="slateblue", alpha=0.2) + 
