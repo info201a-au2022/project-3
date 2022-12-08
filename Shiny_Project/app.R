2ab6e5fea477dfcfd6ce231aa99da3687c1a2b80
Karina <kyb3@uw.edu>
2022-12-07 21:10:35 -0800

    Added to research question answers

--- Shiny_Project/app.R
+++ Shiny_Project/app.R
@@ -5,9 +5,14 @@ library(tidyverse)
 library(maps)
 library(readr)
 library(plotly)
+#library("rsconnect")
 
 source("app_ui.R")
 source("app_server.R")
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+#setAccountInfo(name='karinaa', token='B626E7E2578D2BC98ED2B0BD3933D921', secret='cPFCMcixb1H38uTLQLQKapKaqKL6qDamo5O60dyN')
+
+#deployApp('/Users/karin/info201/project2/project-group-4-section-ab/Shiny_Project')
